127925688fe67eb412e05c191e6f2f9f99a458a6
Saim Al Ifran <phpmysql811@gmail.com>
2025-10-21 09:19:54 +0600

    fix(server): ensure correct app instance is used to register routes

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,5 @@
-import { createServer } from "http";
-import express from "express";
-import { Server } from "socket.io";
-import { initTaskSocket } from "./sockets/task.socket";
-
-const app = express();
-const server = createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",  
-    methods: ["GET", "POST"],
-  },
-});
-
-initTaskSocket(io);
-
-export {io};
+// server.ts
+import { server } from './app';
 
 server.listen(5000, () => {
   console.log("🚀 Server running on http://localhost:5000");
